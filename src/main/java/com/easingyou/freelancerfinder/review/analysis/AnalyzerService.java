449d422173849a20a8adcc5301928965b4563922
josroseboom <josroseboom@gmail.com>
2024-06-11 13:37:40 +0200

    Added dependencies of AnalyzerService

--- src/main/java/com/easingyou/freelancerfinder/review/analysis/AnalyzerService.java
+++ src/main/java/com/easingyou/freelancerfinder/review/analysis/AnalyzerService.java
@@ -4,4 +4,15 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class AnalyzerService {
+
+	private final ReviewAnalyzer reviewAnalyzer;
+	private final SentimentAnalyzer sentimentAnalyzer;
+	private final TrendDetector trendDetector;
+
+	AnalyzerService(ReviewAnalyzer reviewAnalyzer, SentimentAnalyzer sentimentAnalyzer, TrendDetector trendDetector) {
+		this.reviewAnalyzer = reviewAnalyzer;
+		this.sentimentAnalyzer = sentimentAnalyzer;
+		this.trendDetector = trendDetector;
+	}
+
 }
--- src/main/java/com/easingyou/freelancerfinder/review/analysis/TrendDetector.java
+++ src/main/java/com/easingyou/freelancerfinder/review/analysis/TrendDetector.java
@@ -1,4 +1,7 @@
 package com.easingyou.freelancerfinder.review.analysis;
 
+import org.springframework.stereotype.Service;
+
+@Service
 class TrendDetector { /* Identifies trends based on review data */
 }
