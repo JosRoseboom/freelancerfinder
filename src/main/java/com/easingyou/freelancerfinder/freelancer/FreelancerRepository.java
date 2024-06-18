5094bb95ee9440a833ef0b1a320cab1a34537d18
josroseboom <josroseboom@gmail.com>
2024-06-18 13:53:53 +0200

    add FreelancerRepository

--- src/main/java/com/easingyou/freelancerfinder/freelancer/FreelancerRepository.java
+++ src/main/java/com/easingyou/freelancerfinder/freelancer/FreelancerRepository.java
@@ -3,5 +3,8 @@ package com.easingyou.freelancerfinder.freelancer;
 import org.springframework.stereotype.Repository;
 
 @Repository
-interface FreelancerRepository { /* Repository interface */
+interface FreelancerRepository {
+	default Freelancer findById(long freelancerId){
+		return new Freelancer();
+	}
 }
--- src/main/java/com/easingyou/freelancerfinder/freelancer/FreelancerService.java
+++ src/main/java/com/easingyou/freelancerfinder/freelancer/FreelancerService.java
@@ -3,5 +3,6 @@ package com.easingyou.freelancerfinder.freelancer;
 import org.springframework.stereotype.Service;
 
 @Service
-public class FreelancerService { /* Freelancer service logic */
+public class FreelancerService {
+
 }
