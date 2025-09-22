4645043d27bd56784a3620286486f240c20f523c
josroseboom <josroseboom@gmail.com>
2025-09-22 10:59:20 +0200

    Java 25

--- src/main/java/com/easingyou/freelancerfinder/FreelancerFinderApp.java
+++ src/main/java/com/easingyou/freelancerfinder/FreelancerFinderApp.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 class FreelancerFinderApp {
 
-	public static void main(String[] args) {
+	static void main(String[] args) {
 		SpringApplication.run(FreelancerFinderApp.class, args);
 	}
 
