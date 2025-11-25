d063332f7ebb2081808799e8ef2b45916d98255b
josroseboom <josroseboom@gmail.com>
2025-11-25 14:22:22 +0100

    Demo's issue in IntelliJ 'Spring Modulith support 'Allowed dependencies' check ignores shared Modules', see https://youtrack.jetbrains.com/issue/IDEA-381001

--- src/main/java/com/easingyou/freelancerfinder/FreelancerFinderApp.java
+++ src/main/java/com/easingyou/freelancerfinder/FreelancerFinderApp.java
@@ -2,8 +2,10 @@ package com.easingyou.freelancerfinder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.modulith.Modulithic;
 
 @SpringBootApplication
+@Modulithic(sharedModules = "review")
 class FreelancerFinderApp {
 
 	static void main(String[] args) {
--- src/main/java/com/easingyou/freelancerfinder/bidding/BidService.java
+++ src/main/java/com/easingyou/freelancerfinder/bidding/BidService.java
@@ -2,6 +2,12 @@ package com.easingyou.freelancerfinder.bidding;
 
 import org.springframework.stereotype.Service;
 
+import com.easingyou.freelancerfinder.freelancer.FreelancerService;
+import com.easingyou.freelancerfinder.review.ReviewService;
+
 @Service
 public class BidService { /* Bid service logic */
+
+	private FreelancerService freelancerService;
+	private ReviewService reviewService;
 }
